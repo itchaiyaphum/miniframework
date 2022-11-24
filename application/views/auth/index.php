89b63cf0c5bfef763cb0e3134cd086a67dd6bec6
itchaiyaphum <39511454+itchaiyaphum@users.noreply.github.com>
2022-11-24 08:57:06 +0700

    auto format html

--- application/views/auth/index.php
+++ application/views/auth/index.php
@@ -1,64 +1,49 @@
     <div class="container">
-      <div class="row text-center mt-5">
-        <div class="col">
-          <div class="p-2">
-            <img
-              style="width: 50px"
-              src="/assets/img/workflow-mark-indigo-600.svg"
-            />
-          </div>
-          <h2>ลงชื่อเข้าสู่ระบบ</h2>
-          <p>
-            หรือ
-            <a href="/auth_register.php" class=""> สมัครสมาชิกใหม่ </a>
-          </p>
-        </div>
-      </div>
-      
-      <div class="row mt-5 justify-content-center">
-        <div class="col-md-8 col-lg-6">
-          <div class="card">
-            <div class="card-body">
-                <?php echo validation_errors(); ?>
-              <form id="authForm" method="post" action="/auth_login.php">
-                <div class="mb-3">
-                  <label for="inputEmail" class="form-label"
-                    >อีเมล์</label
-                  >
-                  <input
-                    type="email"
-                    class="form-control"
-                    id="inputEmail"
-                    name="email"
-                    value="<?php echo $this->data['email']; ?>"
-                  />
+        <div class="row text-center mt-5">
+            <div class="col">
+                <div class="p-2">
+                    <img style="width: 50px" src="/assets/img/workflow-mark-indigo-600.svg" />
                 </div>
-                <div class="mb-3">
-                  <label for="inputPassword" class="form-label">รหัสผ่าน</label>
-                  <input
-                    type="password"
-                    class="form-control"
-                    id="inputPassword"
-                    name="password"
-                    value="<?php echo $this->data['password']; ?>"
-                  />
-                </div>
-                <div class="d-flex w-100 justify-content-between mb-3">
-                  <div>
-                    <a href="/auth_forget_password.php" class="link-primary">ลืมรหัสผ่าน?</a>
-                  </div>
-                </div>
-
-                <button type="submit" class="btn btn-primary w-100">
-                  ลงชื่อเข้าสู่ระบบ
-                </button>
-              </form>
-              <hr />
-              <div class="text-center">
-                <a href="/" class="link-secondary">กลับหน้าหลัก</a>
-              </div>
+                <h2>ลงชื่อเข้าสู่ระบบ</h2>
+                <p>
+                    หรือ
+                    <a href="/auth_register.php" class=""> สมัครสมาชิกใหม่ </a>
+                </p>
             </div>
-          </div>
         </div>
-      </div>
+
+        <div class="row mt-5 justify-content-center">
+            <div class="col-md-8 col-lg-6">
+                <div class="card">
+                    <div class="card-body">
+                        <?php echo validation_errors(); ?>
+                        <form id="authForm" method="post" action="/auth_login.php">
+                            <div class="mb-3">
+                                <label for="inputEmail" class="form-label">อีเมล์</label>
+                                <input type="email" class="form-control" id="inputEmail" name="email"
+                                    value="<?php echo $this->data['email']; ?>" />
+                            </div>
+                            <div class="mb-3">
+                                <label for="inputPassword" class="form-label">รหัสผ่าน</label>
+                                <input type="password" class="form-control" id="inputPassword" name="password"
+                                    value="<?php echo $this->data['password']; ?>" />
+                            </div>
+                            <div class="d-flex w-100 justify-content-between mb-3">
+                                <div>
+                                    <a href="/auth_forget_password.php" class="link-primary">ลืมรหัสผ่าน?</a>
+                                </div>
+                            </div>
+
+                            <button type="submit" class="btn btn-primary w-100">
+                                ลงชื่อเข้าสู่ระบบ
+                            </button>
+                        </form>
+                        <hr />
+                        <div class="text-center">
+                            <a href="/" class="link-secondary">กลับหน้าหลัก</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
--- application/views/footer.php
+++ application/views/footer.php
@@ -1,3 +1,9 @@
+        <hr />
+        <div class="container text-center">
+            <div>แผนกเทคโนโลยีสารสนเทศ วิทยาลัยเทคนิคชัยภูมิ</div>
+            <div>แข่งขันทักษะประจำปีการศึกษา 2565</div>
+        </div>
         <script src="/assets/js/bootstrap.bundle.min.js"></script>
-    </body>
-</html>
+        </body>
+
+        </html>
