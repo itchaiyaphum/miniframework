bb1778293ad91ef4b7a33752d43e5858c5184222
itchaiyaphum <39511454+itchaiyaphum@users.noreply.github.com>
2022-12-11 11:43:19 +0700

    implement : customer_profile completed

--- application/controllers/customer_profile.php
+++ application/controllers/customer_profile.php
@@ -4,19 +4,40 @@ class Customer_profile extends Controller
 {
     public function index()
     {
-        // check is login
+        // get data from login_form
+        $profile_data = $this->app->input_lib->post();
 
-        // get data from form
+        $firstname = $this->app->input_lib->get_post('firstname', '');
+        $lastname = $this->app->input_lib->get_post('lastname', '');
+        $mobile_no = $this->app->input_lib->get_post('mobile_no', '');
+        $address = $this->app->input_lib->get_post('address', '');
+        $email = $this->app->input_lib->get_post('email', '');
+        $password = $this->app->input_lib->get_post('password', '');
 
-        // set validation rules
+        // set rules for validation data
+        $this->app->form_validation_lib->set_rules('firstname', 'ชื่อ', 'required');
+        $this->app->form_validation_lib->set_rules('lastname', 'นามสกุล', 'required');
+        $this->app->form_validation_lib->set_rules('mobile_no', 'เบอร์โทร', 'required');
+        $this->app->form_validation_lib->set_rules('address', 'ที่อยู่', 'required');
+        $this->app->form_validation_lib->set_rules('email', 'อีเมล์', 'required');
 
         // run validation
-
-        // save profile data to database
-        $this->app->customer_profile_lib->save();
+        if ($this->app->form_validation_lib->run()) {
+            if ($this->app->customer_profile_lib->save($profile_data)) {
+                $this->app->form_validation_lib->set_message('success', 'บันทึกข้อมูลเรียบร้อย');
+            }
+        }
 
         $data = [];
-        $data['title'] = 'แก้ไขข้อมูลส่วนตัว - ระบบสั่งอาหารออนไลน์';
+        $data['title'] = 'ลงทะเบียนสำหรับลูกค้า - ระบบสั่งอาหารออนไลน์';
+        $data['profile'] = $this->app->profile_lib->get_profile();
+
+        $data['firstname'] = $firstname;
+        $data['lastname'] = $lastname;
+        $data['mobile_no'] = $mobile_no;
+        $data['address'] = $address;
+        $data['email'] = $email;
+        $data['password'] = $password;
 
         $this->app->view('header', $data);
         $this->app->view('nav', $data);
--- application/views/customer/profile/profile.php
+++ application/views/customer/profile/profile.php
@@ -1,6 +1,4 @@
-<div class="container mt-3">
-    <?php echo validation_errors(); ?>
-    <?php echo action_messages(); ?>
+<div class="container mt-5">
     <div class="row justify-content-center">
         <div class="col-md-8 col-lg-6">
             <div class="card mb-5">
@@ -8,13 +6,15 @@
                     <h2 class="card-title h4">แก้ไขข้อมูลส่วนตัว</h2>
                 </div>
                 <div class="card-body">
-                    <form id="profileForm" class="needs-validation" method="post" novalidate>
+                    <?php echo validation_errors(); ?>
+                    <?php echo action_messages(); ?>
+                    <form id="mainForm" method="post" enctype="multipart/form-data">
                         <!-- field: firstname -->
                         <div class="row mb-4">
                             <label for="firstnameLabel" class="col-sm-3 col-form-label form-label">ชื่อ *</label>
                             <div class="col-sm-9">
-                                <input type="text" class="form-control" name="firstname" id="firstnameLabel" value=""
-                                    required />
+                                <input type="text" class="form-control" name="firstname" id="firstnameLabel"
+                                    value="<?php echo $this->data['profile']->firstname; ?>" required />
                             </div>
                         </div>
                         <!-- End field: firstname -->
@@ -23,8 +23,8 @@
                         <div class="row mb-4">
                             <label for="lastnameLabel" class="col-sm-3 col-form-label form-label">นามสกุล *</label>
                             <div class="col-sm-9">
-                                <input type="text" class="form-control" name="lastname" id="lastnameLabel" value=""
-                                    required />
+                                <input type="text" class="form-control" name="lastname" id="lastnameLabel"
+                                    value="<?php echo $this->data['profile']->lastname; ?>" required />
                             </div>
                         </div>
                         <!-- End field: lastname -->
@@ -36,7 +36,7 @@
 
                             <div class="col-sm-9">
                                 <input type="text" class="js-input-mask form-control" name="mobile_no" id="phoneLabel"
-                                    value="" required />
+                                    value="<?php echo $this->data['profile']->mobile_no; ?>" required />
                             </div>
                         </div>
                         <!-- end field: mobile_no -->
@@ -47,18 +47,19 @@
                                 *</label>
 
                             <div class="col-sm-9">
-                                <textarea row="4" class="form-control" name="address" id="addressLine1Label"></textarea>
+                                <textarea row="4" class="form-control" name="address" id="addressLine1Label"
+                                    required><?php echo $this->data['profile']->address; ?></textarea>
                             </div>
                         </div>
                         <!-- end field: address -->
 
                         <!-- field: thumbnail -->
                         <div class="row mb-4">
-                            <label for="thumbnailLabel"
-                                class="col-sm-3 col-form-label form-label">รูปภาพประจำตัว</label>
+                            <label for="thumbnailLabel" class="col-sm-3 col-form-label form-label">รูปภาพประจำตัว
+                                </label>
                             <div class="col-sm-9">
-                                <img src="/assets/img/admin_index.png" class="mb-3 w-25">
-                                <input type="file" class="form-control" name="thumbnail" value="" required />
+                                <img src="<?php echo $this->data['profile']->thumbnail; ?>" class="mb-3 w-25 border">
+                                <input type="file" class="form-control" name="thumbnail" />
                             </div>
                         </div>
                         <!-- end field: thumbnail -->
@@ -68,8 +69,8 @@
                             <label for="emailLabel" class="col-sm-3 col-form-label form-label">อีเมล์ *</label>
 
                             <div class="col-sm-9">
-                                <input type="email" class="form-control" name="email" id="emailLabel" value=""
-                                    required />
+                                <input type="email" class="form-control" name="email" id="emailLabel"
+                                    value="<?php echo $this->data['profile']->email; ?>" required />
                             </div>
                         </div>
                         <!-- end field: email -->
@@ -79,8 +80,7 @@
                             <label for="passwordLabel" class="col-sm-3 col-form-label form-label">รหัสผ่าน</label>
 
                             <div class="col-sm-9">
-                                <input type="password" class="form-control" name="password" id="passwordLabel"
-                                    value="" />
+                                <input type="password" class="form-control" name="password" id="passwordLabel"/>
                             </div>
                         </div>
                         <!-- end field: password -->
@@ -95,9 +95,11 @@
                             </div>
                         </div>
 
-
-                        <input type="hidden" name="id" value="0" />
                     </form>
+                    <hr />
+                    <div class="text-center">
+                        <a href="/" class="link-secondary">กลับหน้าหลัก</a>
+                    </div>
                 </div><!-- end: card-body -->
             </div><!-- end: card -->
         </div>
